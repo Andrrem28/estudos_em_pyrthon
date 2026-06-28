33757e463821cf56ca1ec15d676222517f89ab27
André Martins <andree.martins_@hotmail.com>
2026-06-28 10:47:55 -0300

    criado módulos de exemplos para documentação em Python

---
+++ aulas/modulo_3/exp_documentacao/main.py
@@ -0,0 +1,15 @@
+# import uma_linha
+# import varias_linhas
+# import documentando_funcoes
+import documentando_class
+
+# print(dir(uma_linha))
+# print(uma_linha.__doc__)
+# print(uma_linha.__file__)
+# print(uma_linha.__name__)
+# print(uma_linha.__doc__)
+
+# help(uma_linha)
+# help(varias_linhas)
+# help(documentando_funcoes)
+help(documentando_class)
---
+++ aulas/modulo_3/exp_documentacao/varias_linhas.py
@@ -0,0 +1,8 @@
+"""
+Aqui é o título da minha documentação do meu módulo.
+
+Aqui fica a parte da descrição do meu módulo.
+Aqui fica a parte da descrição do meu módulo.
+Aqui fica a parte da descrição do meu módulo.
+
+"""
